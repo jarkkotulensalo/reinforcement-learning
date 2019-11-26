7659c4d86d248036ea9938a18584ee5610d573de
Jarkko Tulensalo <jarkko.tulensalo@hotmail.com>
2019-11-26 21:22:27 +0200

    replay memory with dagger preprocessing

--- test_pong_ai_multi.py
+++ test_pong_ai_multi.py
@@ -10,6 +10,7 @@ import numpy as np
 import argparse
 import wimblepong
 from PIL import Image
+import agent_jack
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--headless", action="store_true", help="Run in headless mode")
@@ -23,34 +24,32 @@ env.unwrapped.scale = args.scale
 env.unwrapped.fps = args.fps
 # Number of episodes/games to play
 episodes = 100000
-
+LOADPATH = "./weights_Jack-v0_100000.mdl"
 # Define the player IDs for both SimpleAI agents
 player_id = 1
 opponent_id = 3 - player_id
 opponent = wimblepong.SimpleAi(env, opponent_id)
-player = wimblepong.SimpleAi(env, player_id)
+player = agent_jack(env, player_id)
 
+print(player.get_name())
+player.load_policy(LOADPATH)
 # Set the names for both SimpleAIs
 env.set_names(player.get_name(), opponent.get_name())
 
 win1 = 0
 for i in range(0,episodes):
     done = False
+    ob1,ob2 = env.reset()
     while not done:
         # Get the actions from both SimpleAIs
-        action1 = player.get_action()
+        action1 = player.get_action(ob1, eps=0.0)
         action2 = opponent.get_action()
         # Step the environment and get the rewards and new observations
-        (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
-        #img = Image.fromarray(ob1)
-        #img.save("ob1.png")
-        #img = Image.fromarray(ob2)
-        #img.save("ob2.png")
-        # Count the wins
+        (ob1_next, ob2), (rew1, rew2), done, info = env.step((action1, action2))
+        ob1 = ob1_next
         if rew1 == 10:
             win1 += 1
         if not args.headless:
             env.render()
         if done:
-            observation= env.reset()
             print("episode {} over. Broken WR: {:.3f}".format(i, win1/(i+1)))
